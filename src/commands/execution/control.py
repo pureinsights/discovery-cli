4618b8aac68f6514dddec2b79727b940901bc738
aaronvvargas <123981431+aaronvvargas@users.noreply.github.com>
2023-05-05 10:52:33 -0600

    PDP-1194: Implementing the control command (#32)
    * Adding the implementation, unit testing and documentation about file delete command
    
    * Adding examples about the file delete command
    
    * Implementation, unit testing and documentation about ls command
    
    * Solving an error with a snapshot
    
    * Adding the implementation, unit testing and documentation about start command
    
    * Revert "Adding the implementation, unit testing and documentation about start command"
    
    This reverts commit c5270db7b595026d74c772c1a9e16856a8b0865f.
    
    * Adding the implementation, unit testing and documentation about the start execution command
    
    * Adding the implementation of the reset command
    
    * Adding the unit testing and documentation of the rest command
    
    * Adding the implementation, unit testing and documentation of the control command
    
    ---------
    
    Co-authored-by: Pablo Arce <97907316+parce-pi@users.noreply.github.com>

---
+++ src/commands/execution/control.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2023 Pureinsights Technology Ltd. All rights reserved.
+#
+#  Permission to use, copy, modify or distribute this software and its
+#  documentation for any purpose is subject to a licensing agreement with
+#  Pureinsights Technology Ltd.
+#
+#  All information contained within this file is the property of
+#  Pureinsights Technology Ltd. The distribution or reproduction of this
+#  file or any information contained within is strictly forbidden unless
+#  prior written permission has been granted by Pureinsights Technology Ltd.
+import json
+
+import click
+
+from commons.console import print_console
+from commons.constants import INGESTION, URL_SEED_CONTROL
+from commons.http_requests import put
+
+
+def run(config: dict, seed: str, action: str):
+  """
+  Will control the execution for the given seed id.
+  :param dict config: The configuration containing the url products.
+  :param str seed: The id of the seed to start the execution.
+  :param str action: The action to trigger.
+  """
+  res = put(URL_SEED_CONTROL.format(config[INGESTION], id=seed), params={"action": action})
+  if json.loads(res).get("acknowledged", False):
+    print_console(
+      f"The seed {click.style(seed, fg='green')} was {click.style(f'{action.upper()}', fg='magenta')} successfully."
+    )
+    return
+  print_console(f"Couldn't {click.style(action.upper(), fg='magenta')} the seed {click.style(seed, fg='green')}.")
