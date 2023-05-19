a56cbdcc3fb4be46fcccd3de8dcf7da9632ea6e8
aaronvvargas <123981431+aaronvvargas@users.noreply.github.com>
2023-05-19 09:48:27 -0600

    PDP-1242: Implementation of the Staging Bucket Get command (#39)
    * Adding the implementation, unit testing and documentation of the staging item add command.
    
    * Deleteing an unecesary file
    
    * Solving smell codes
    
    * Adding the implementation, unit testing and documentation of the staging item get command.
    
    * Creating the id of the item if one was not provided.
    
    * Adding examples of use to the user.md
    
    * Updating snapshots
    
    * Updating snapshots
    
    * Mocking the uuid generator in a test
    
    * Adding the implementation, unti testing and documentation of the staging bucket get command
    
    * Enabling filters and pagination

--- tests/commands/staging/staging_command_test.py
+++ tests/commands/staging/staging_command_test.py
@@ -12,6 +12,93 @@ from pdp import pdp
 from pdp_test import cli
 
 
+def test_get_bucket(mocker, snapshot):
+  """
+  Test the command defined in :func:`src.commands.staging.bucket.command.get`.
+  """
+  mocker.patch('commands.staging.bucket.get.get', return_value=b'{'
+                                                               b'"transactionId": "645e9901c8e408abf7e1a194",'
+                                                               b'"timestamp": "2023-05-12T19:52:33.245Z",'
+                                                               b'"action": "ADD",'
+                                                               b'"bucket": "test",'
+                                                               b'"contentId": "test3",'
+                                                               b'"checksum": "23b5c58d597754037351ebdc5497882b"'
+                                                               b'}')
+  response = cli.invoke(pdp, ["staging", "bucket", "get", "--bucket", "fake"])
+  assert response.exit_code == 0
+  snapshot.assert_match(response.output, 'test_get_bucket.snapshot')
+
+
+def test_get_bucket_filter(mocker, snapshot):
+  """
+  Test the command defined in :func:`src.commands.staging.bucket.command.get`,
+  when the user wants to filter the data.
+  """
+  mock_post = mocker.patch(
+    'commands.staging.bucket.get.post',
+    return_value=b'{'
+                 b'"transactionId": "645e9901c8e408abf7e1a194",'
+                 b'"timestamp": "2023-05-12T19:52:33.245Z",'
+                 b'"action": "ADD",'
+                 b'"bucket": "test",'
+                 b'"contentId": "test3",'
+                 b'"checksum": "23b5c58d597754037351ebdc5497882b"'
+                 b'}'
+  )
+  response = cli.invoke(pdp, ["staging", "bucket", "get", "--bucket", "fake", "--page", "2", "--token", "fake-token",
+                              "--content-type",
+                              "CONTENT"])
+  assert response.exit_code == 0
+  mock_post.assert_called_once_with(
+    'http://localhost:8081/content/fake/filter',
+    query_params={'token': 'fake-token', 'contentType': 'CONTENT', 'size': None}
+  )
+  snapshot.assert_match(response.output, 'test_get_bucket_filter.snapshot')
+
+
+def test_get_bucket_pagination(mocker, snapshot):
+  """
+  Test the command defined in :func:`src.commands.staging.bucket.command.get`,
+  when the user wants to use pagination on the data.
+  """
+  mock_post = mocker.patch(
+    'commands.staging.bucket.get.post',
+    return_value=b'{'
+                 b'"transactionId": "645e9901c8e408abf7e1a194",'
+                 b'"timestamp": "2023-05-12T19:52:33.245Z",'
+                 b'"action": "ADD",'
+                 b'"bucket": "test",'
+                 b'"contentId": "test3",'
+                 b'"checksum": "23b5c58d597754037351ebdc5497882b"'
+                 b'}'
+  )
+  response = cli.invoke(pdp, ["staging", "bucket", "get", "--bucket", "fake", "--page", "2", "--asc",
+                              "CONTENT", "--desc", "fake-property"])
+  assert response.exit_code == 0
+  mock_post.assert_called_once_with(
+    'http://localhost:8081/content/fake/query',
+    query_params={'page': '2', 'sort': ['CONTENT,asc', 'fake-property,desc'], 'size': None}
+  )
+  snapshot.assert_match(response.output, 'test_get_bucket_filter.snapshot')
+
+
+def test_get_bucket_json(mocker, snapshot):
+  """
+  Test the command defined in :func:`src.commands.staging.bucket.command.get`.
+  """
+  mocker.patch('commands.staging.bucket.get.get', return_value=b'{'
+                                                               b'"transactionId": "645e9901c8e408abf7e1a194",'
+                                                               b'"timestamp": "2023-05-12T19:52:33.245Z",'
+                                                               b'"action": "ADD",'
+                                                               b'"bucket": "test",'
+                                                               b'"contentId": "test3",'
+                                                               b'"checksum": "23b5c58d597754037351ebdc5497882b"'
+                                                               b'}')
+  response = cli.invoke(pdp, ["staging", "bucket", "get", "--bucket", "fake", "--json"])
+  assert response.exit_code == 0
+  snapshot.assert_match(response.output, 'test_get_bucket_json.snapshot')
+
+
 def test_add_item(mocker, snapshot):
   """
   Test the command defined in :func:`src.commands.staging.item.command.add`.
