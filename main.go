b58836ea3bd4b105806da3399c3f07191531a279
diegogranados-pi <diego.granados@pureinsights.com>
2025-08-11 14:04:30 -0600

    PDP-3241: Test with higher coverage

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "fmt"
-
 // Add returns the sum of two integers.
 func Add(a, b int) int {
 	return a + b
 }
 
 func main() {
-	fmt.Println("Hello from Go!")
-	fmt.Printf("2 + 3 = %d\n", Add(2, 3))
+	Add(2, 3)
 }
