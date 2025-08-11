1a020d5abfc4fa900fd7e37b8781124e37d07912
diegogranados-pi <diego.granados@pureinsights.com>
2025-08-11 10:28:54 -0600

    PDP-3241: Add pull request workflow

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import "fmt"
+
+// Add returns the sum of two integers.
+func Add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Hello from Go!")
+	fmt.Printf("2 + 3 = %d\n", Add(2, 3))
+}
