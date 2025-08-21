f13ced8f4fba7aa38d4af6092371337c7dfae91e
diegogranados-pi <diego.granados@pureinsights.com>
2025-08-21 17:59:09 -0600

    PDP-3235: Start on summarizer

---
+++ discovery/summarizer.go
@@ -0,0 +1,31 @@
+package discovery
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/tidwall/gjson"
+)
+
+type summarizer struct {
+	client
+}
+
+func (s summarizer) Summarize(seedId, executionId uuid.UUID) (gjson.Result, error) {
+	jobSummaryExecution, err := execute(s.client, http.MethodGet, "/"+seedId.String()+"/execution/"+executionId.String()+"/job/summary")
+	if err != nil {
+		return gjson.Result{}, err
+	}
+
+	recordSummaryExecution, err := execute(s.client, http.MethodGet, "/"+seedId.String()+"/execution/"+executionId.String()+"/job/summary")
+	if err != nil {
+		return gjson.Result{}, err
+	}
+
+	recordSummarySeed, err := execute(s.client, http.MethodGet, "/"+seedId.String()+"/execution/"+executionId.String()+"/job/summary")
+	if err != nil {
+		return gjson.Result{}, err
+	}
+
+	return response, nil
+}
