580506c3ef7ece36ce67e1f0aab1b313a939ae28
diegogranados-pi <diego.granados@pureinsights.com>
2025-08-21 13:37:32 -0600

    PDP-3234: Start on searcher

--- discovery/searcher.go
+++ discovery/searcher.go
@@ -3,19 +3,39 @@ package discovery
 import (
 	"net/http"
 
-	"github.com/google/uuid"
 	"github.com/tidwall/gjson"
 )
 
-type cloner struct {
+type searcher struct {
 	client
 }
 
-func (c cloner) Clone(id uuid.UUID, params map[string][]string) (gjson.Result, error) {
-	response, err := execute(c.client, http.MethodPost, "/"+id.String()+"/clone", WithQueryParameters(params))
-	if err != nil {
-		return gjson.Result{}, err
-	} else {
+func (c cloner) Search(filter gjson.Result, params map[string][]string) ([]gjson.Result, error) {
+	response, err := execute(c.client, http.MethodPost, "/search", WithQueryParameters(params))
+	if err == nil {
+		elementNumber := response.Get("numberOfElements").Int()
+		pageNumber := response.Get("pageNumber").Int()
+		totalPages := response.Get("totalPages").Int()
+		totalSize := response.Get("totalSize").Int()
+		elements := response.Get("content").Array()
+		for pageNumber < totalPages || elementNumber < totalSize {
+			response, err = execute(c.client, http.MethodPost, "/search", WithQueryParameters(params))
+			if err == nil {
+				pageElementNumber := response.Get("numberOfElements").Int()
+				pagePageNumber := response.Get("pageNumber").Int()
+				pageElements := response.Get("content").Array()
+			} else {
+
+				return []gjson.Result{}, err
+			}
+		}
 		return response, nil
+	} else {
+
+		return []gjson.Result{}, err
 	}
+}
+
+func Autocomplete(q string) ([]gjson.Result, error) {
+
 }
