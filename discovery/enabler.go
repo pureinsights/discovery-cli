c2f1284d4b2b487710bc2ec58b4118feec0268be
diegogranados-pi <diego.granados@pureinsights.com>
2025-08-25 10:50:41 -0600

    PDP-3236: Add docs

--- discovery/enabler.go
+++ discovery/enabler.go
@@ -7,10 +7,12 @@ import (
 	"github.com/tidwall/gjson"
 )
 
+// Enabler is a struct that adds the path to enable or disable Queryflow endpoints and Ingestion Seed Schedules.
 type enabler struct {
 	client
 }
 
+// Enable runs a request to enable an entity. The client's URL needs to already have the path that points to the correct type of entity.
 func (e enabler) Enable(id uuid.UUID) (gjson.Result, error) {
 	enabled, err := execute(e.client, http.MethodPatch, "/"+id.String()+"/enable")
 	if err != nil {
@@ -20,6 +22,7 @@ func (e enabler) Enable(id uuid.UUID) (gjson.Result, error) {
 	return enabled, nil
 }
 
+// Disable runs a request to disable an entity. The client's URL needs to already have the path that points to the correct type of entity.
 func (e enabler) Disable(id uuid.UUID) (gjson.Result, error) {
 	disabled, err := execute(e.client, http.MethodPatch, "/"+id.String()+"/disable")
 	if err != nil {
