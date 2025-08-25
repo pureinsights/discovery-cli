769bd2409016d2a2f237f52c27195201e7efb08c
diegogranados-pi <diego.granados@pureinsights.com>
2025-08-25 17:53:51 -0600

    PDP-3239: Start with constructors

--- discovery/core.go
+++ discovery/core.go
@@ -1,35 +1,120 @@
 package discovery
 
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/tidwall/gjson"
+)
+
 type labelsClient struct {
-    crud
+	crud
+}
+
+func newLabelsClient(core client) labelsClient {
+	return labelsClient{
+		crud{
+			getter{
+				client: newSubClient(core, "/label"),
+			},
+		},
+	}
 }
 
 type secretsClient struct {
-    crud
+	crud
+}
+
+func newSecretsClient(core client) secretsClient {
+	return secretsClient{
+		crud{
+			getter{
+				client: newSubClient(core, "/secret"),
+			},
+		},
+	}
 }
 
 type credentialsClient struct {
-    crud
-    searcher
-    cloner
+	crud
+	searcher
+	cloner
+}
+
+func newCredentialsClient(core client) credentialsClient {
+	client := newSubClient(core, "/credential")
+	return credentialsClient{
+		crud: crud{
+			getter{
+				client: client,
+			},
+		},
+		searcher: searcher{
+			client: client,
+		},
+		cloner: cloner{
+			client: client,
+		},
+	}
 }
 
 type serversClient struct {
-    crud
-    cloner
-    searcher
+	crud
+	searcher
+	cloner
 }
-Ping(id uuid.UUID) serverClient
+
+func newServersClient(core client) serversClient {
+	client := newSubClient(core, "/server")
+	return serversClient{
+		crud: crud{
+			getter{
+				client: client,
+			},
+		},
+		searcher: searcher{
+			client: client,
+		},
+		cloner: cloner{
+			client: client,
+		},
+	}
+}
+
+func Ping(id uuid.UUID) serversClient
 
 type filesClient struct {
-    client
+	client
 }
-Upload() ({JsonObject}, error)
-Retrieve(key string) ([]byte, error)
-List() ({JsonArray}, error)
-Delete() ({JsonObject}, error)
+
+func newFilesClient(core client) filesClient {
+	client := newSubClient(core, "/file")
+	return filesClient{
+		client: client,
+	}
+}
+
+func (fc filesClient) Upload(key, file string) (gjson.Result, error) {
+	response, err := execute(fc.client, http.MethodPut, key, WithFile(file))
+	if err != nil {
+		return gjson.Result{}, err
+	}
+
+	return response, nil
+}
+
+func Retrieve(key string) ([]byte, error)
+func List() ([]gjson.Result, error)
+func Delete() (gjson.Result, error)
 
 type maintenanceClient struct {
-     client
+	client
 }
-Log(componentName, level, loggerName string) ({JsonArray}, error)
+
+func newMaintenanceClient(core client) maintenanceClient {
+	return maintenanceClient{
+		client: newSubClient(core, "/maintenance"),
+	}
+}
+
+func Log(componentName, level, loggerName string) error
