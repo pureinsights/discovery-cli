6e73ddca6af0918c01cd6f9f1faa59a4b69c928c
diegogranados-pi <diego.granados@pureinsights.com>
2025-08-27 14:19:39 -0600

    PDP-3239: Ping unit test

--- discovery/core_test.go
+++ discovery/core_test.go
@@ -13,15 +13,15 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-// TestCore has table-driven tests to test the cloner methods.
-func TestCore(t *testing.T) {
+// TestCore has table-driven tests to test the core methods.
+func Test_serversClient_Ping(t *testing.T) {
 	tests := []struct {
 		name       string
 		method     string
 		path       string
 		statusCode int
 		response   string
-		testFunc   func(t *testing.T, c cloner)
+		testFunc   func(t *testing.T, sc serversClient)
 	}{
 		// Working case
 		{
@@ -32,12 +32,11 @@ func TestCore(t *testing.T) {
 			response: `{
 			"acknowledged": true
 			}`,
-			testFunc: func(t *testing.T, c cloner) {
+			testFunc: func(t *testing.T, sc serversClient) {
 				id := uuid.MustParse("f6950327-3175-4a98-a570-658df852424a")
-				response, err := c.Clone(id, map[string][]string{"name": {"mongo2"}})
+				response, err := sc.Ping(id)
 				require.NoError(t, err)
-				assert.Equal(t, "mongo2", response.Get("name").String())
-				assert.Equal(t, "mongo-secret", response.Get("secret").String())
+				assert.True(t, response.Get("acknowledged").Bool())
 			},
 		},
 
@@ -55,34 +54,35 @@ func TestCore(t *testing.T) {
 					],
 					"timestamp": "2025-08-26T20:42:26.372708600Z"
 			}`,
-			testFunc: func(t *testing.T, c cloner) {
-				id := uuid.MustParse("5f125024-1e5e-4591-9fee-365dc20eeeed")
-				response, err := c.Clone(id, map[string][]string{"name": {"mongo2"}})
+			testFunc: func(t *testing.T, sc serversClient) {
+				id := uuid.MustParse("f6950327-3175-4a98-a570-658df852424a")
+				response, err := sc.Ping(id)
 				assert.Equal(t, gjson.Result{}, response)
-				assert.EqualError(t, err, fmt.Sprintf("status: %d, body: %s", http.StatusNotFound, []byte(`{
+				assert.EqualError(t, err, fmt.Sprintf("status: %d, body: %s", http.StatusBadGateway, `{
 					"status": 502,
 					"code": 8002,
 					"messages": [
 							"An error occurred while pinging the Mongo client."
 					],
 					"timestamp": "2025-08-26T20:42:26.372708600Z"
-			}`)))
+			}`))
 			},
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			srv := httptest.NewServer(http.HandlerFunc(
-				testutils.HttpHandler(func(r *http.Request) {
-					assert.Equal(t, tc.method, r.Method)
-					assert.Equal(t, tc.path, r.URL.Path)
-				},
-					tc.statusCode, "application/json", tc.response)))
+			srv := httptest.NewServer(
+				testutils.HttpHandler(t,
+					tc.statusCode, "application/json", tc.response, func(t *testing.T, r *http.Request) {
+						assert.Equal(t, tc.method, r.Method)
+						assert.Equal(t, tc.path, r.URL.Path)
+					}))
 			defer srv.Close()
 
-			c := cloner{client: newClient(srv.URL, "")}
-			tc.testFunc(t, c)
+			coreClient := newClient(srv.URL, "")
+			serverClient := newServersClient(coreClient)
+			tc.testFunc(t, serverClient)
 		})
 	}
 }
