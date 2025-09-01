51296d5707ba6c42dd348670a6d94cfa7551a29c
diegogranados-pi <diego.granados@pureinsights.com>
2025-09-01 10:55:37 -0600

    PDP-3243: Add assertions

--- discovery/queryflow_test.go
+++ discovery/queryflow_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Test_newQueryFlowProcessorsClient test the QueryFlowProcessorsClient's constructor
+// Test_newQueryFlowProcessorsClient test the queryFlowProcessorsClient's constructor
 func Test_newQueryFlowProcessorsClient(t *testing.T) {
 	url := "http://localhost:8088/v2"
 	apiKey := "Api Key"
@@ -14,6 +14,8 @@ func Test_newQueryFlowProcessorsClient(t *testing.T) {
 
 	assert.Equal(t, apiKey, qpc.crud.client.ApiKey)
 	assert.Equal(t, url+"/processor", qpc.crud.client.client.BaseURL)
+	assert.Equal(t, apiKey, qpc.cloner.client.ApiKey)
+	assert.Equal(t, url+"/processor", qpc.cloner.client.client.BaseURL)
 }
 
 // Test_newEndpointsClient tests the constructor of endpointsClients.
@@ -24,4 +26,6 @@ func Test_newEndpointsClient(t *testing.T) {
 
 	assert.Equal(t, apiKey, qec.crud.client.ApiKey)
 	assert.Equal(t, url+"/endpoint", qec.crud.client.client.BaseURL)
+	assert.Equal(t, apiKey, qec.cloner.client.ApiKey)
+	assert.Equal(t, url+"/endpoint", qec.cloner.client.client.BaseURL)
 }
