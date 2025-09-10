dd3a21fc6fe388d39505abb027a5906c725a269a
diegogranados-pi <diego.granados@pureinsights.com>
2025-09-10 10:15:45 -0600

    PDP-3259: Add error

--- internal/cli/error.go
+++ internal/cli/error.go
@@ -1,1 +1,29 @@
 package cli
+
+import "fmt"
+
+type ExitCode int
+
+type Error struct {
+	ExitCode ExitCode
+	Message  string
+	Cause    error
+}
+
+func (e Error) Error() string {
+	if e.Cause != nil {
+		return fmt.Sprintf("Message: %s\nCause: %s", e.Message, e.Cause.Error())
+	} else {
+		return fmt.Sprintf("Message: %s", e.Message)
+	}
+}
+
+const (
+	SuccessExitCode    ExitCode = 0
+	ErrorExitCode      ExitCode = 1
+	PanicErrorExitCode ExitCode = 2
+)
+
+func NewErrorWithCause(code int, cause error, message string, args ...any) Error
+
+func NewError(code int, message string, args ...any) Error
