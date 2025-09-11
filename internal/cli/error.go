15193a9d1af77b30a46d9bb7a423637acaf46c9f
diegogranados-pi <diego.granados@pureinsights.com>
2025-09-11 09:36:53 -0600

    PDP-3259: Add docs

--- internal/cli/error.go
+++ internal/cli/error.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 )
 
+// ExitCode is a type definition that is used to define the possible execution exit codes.
 type ExitCode int
 
+// Error is a struct that wraps any error that can occur with the CLI and Discovery in order to improve their traceability.
 type Error struct {
 	ExitCode ExitCode
 	Message  string
 	Cause    error
 }
 
+// Error() defines how the error struct is formatted into a string.
+// This is done to comply with the error interface.
 func (e Error) Error() string {
 	if e.Cause != nil {
 		return fmt.Sprintf("Message: %s\nCause: %s", e.Message, e.Cause.Error())
@@ -21,12 +25,14 @@ func (e Error) Error() string {
 	}
 }
 
+// The following constants represent possible exit codes of the CLI.
 const (
-	SuccessExitCode    ExitCode = 0
-	ErrorExitCode      ExitCode = 1
-	PanicErrorExitCode ExitCode = 2
+	SuccessExitCode    ExitCode = 0 // This code is used when the CLI finished the command successfully.
+	ErrorExitCode      ExitCode = 1 // This code is used when the CLI failed due to a normal error.
+	PanicErrorExitCode ExitCode = 2 // This code is used when the LCI failed because it panicked somewhere in the code.
 )
 
+// NewErrorWithCause creates an Error with a cause. It receives the exit code, cause, message, and any arguments that can be added to the message in a formatted string.
 func NewErrorWithCause(code ExitCode, cause error, message string, args ...any) Error {
 	return Error{
 		ExitCode: code,
@@ -35,6 +41,7 @@ func NewErrorWithCause(code ExitCode, cause error, message string, args ...any) 
 	}
 }
 
+// NewErrorWithCause creates an Error with a nil cause. It receives the exit code, message, and any arguments that can be added to the message in a formatted string.
 func NewError(code ExitCode, message string, args ...any) Error {
 	return Error{
 		ExitCode: code,
@@ -43,6 +50,7 @@ func NewError(code ExitCode, message string, args ...any) Error {
 	}
 }
 
+// FromError transforms an error into the Error struct.
 func FromError(err error) Error {
 	var e Error
 	if errors.As(err, &e) {
--- internal/cli/error_test.go
+++ internal/cli/error_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestError_Error tests the Error.Error() function that outputs a string.
 func TestError_Error(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -40,6 +41,7 @@ func TestError_Error(t *testing.T) {
 	}
 }
 
+// TestNewErrorWithCause tests the NewErrorWithCause constructor function
 func TestNewErrorWithCause(t *testing.T) {
 	tests := []struct {
 		name          string
@@ -83,17 +85,110 @@ func TestNewErrorWithCause(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := NewErrorWithCause(tt.code, tt.cause, tt.msg, tt.args...)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := NewErrorWithCause(tc.code, tc.cause, tc.msg, tc.args...)
 
-			assert.Equal(t, tt.expectedCode, got.ExitCode)
-			assert.Equal(t, tt.expectedMsg, got.Message)
+			assert.Equal(t, tc.expectedCode, got.ExitCode)
+			assert.Equal(t, tc.expectedMsg, got.Message)
 
-			if tt.expectedCause != nil {
-				assert.EqualError(t, got.Cause, tt.expectedCause.Error())
+			if tc.expectedCause != nil {
+				assert.EqualError(t, got.Cause, tc.expectedCause.Error())
 			} else {
 				assert.Nil(t, got.Cause)
+			}
+		})
+	}
+}
+
+// TestNewError test the NewError() constructor function.
+func TestNewError(t *testing.T) {
+	tests := []struct {
+		name         string
+		code         ExitCode
+		msg          string
+		args         []any
+		expectedCode ExitCode
+		expectedMsg  string
+	}{
+		{
+			name:         "Test with args",
+			code:         ErrorExitCode,
+			msg:          "Error %s",
+			args:         []any{"connecting to Discovery"},
+			expectedCode: ErrorExitCode,
+			expectedMsg:  "Error connecting to Discovery",
+		},
+		{
+			name:         "Test with no args",
+			code:         PanicErrorExitCode,
+			msg:          "Error connecting to Discovery",
+			args:         []any(nil),
+			expectedCode: PanicErrorExitCode,
+			expectedMsg:  "Error connecting to Discovery",
+		},
+		{
+			name:         "Test with empty message",
+			code:         SuccessExitCode,
+			msg:          "",
+			args:         nil,
+			expectedCode: SuccessExitCode,
+			expectedMsg:  "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := NewError(tc.code, tc.msg, tc.args...)
+
+			assert.Equal(t, tc.expectedCode, got.ExitCode)
+			assert.Equal(t, tc.expectedMsg, got.Message)
+			assert.Nil(t, got.Cause)
+		})
+	}
+}
+
+// TestFromError tests the FromError() function that converts any error to the Error struct.
+func TestFromError(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         error
+		expectedCode  ExitCode
+		expectedMsg   string
+		expectedCause string
+	}{
+		{
+			name: "Input is already Error (value)",
+			input: Error{
+				ExitCode: ErrorExitCode,
+				Message:  "An error occurred",
+				Cause:    errors.New("Unable to connect to Discovery"),
+			},
+			expectedCode:  ErrorExitCode,
+			expectedMsg:   "An error occurred",
+			expectedCause: "Unable to connect to Discovery",
+		},
+		{
+			name:          "input is generic error",
+			input:         errors.New("JSON unmarshal failed"),
+			expectedCode:  ErrorExitCode,
+			expectedMsg:   "",
+			expectedCause: "JSON unmarshal failed",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := FromError(tc.input)
+
+			assert.Equal(t, tc.expectedCode, got.ExitCode)
+			assert.Equal(t, tc.expectedMsg, got.Message)
+
+			if tc.expectedCause == "" {
+				assert.Nil(t, got.Cause)
+			} else {
+				assert.NotNil(t, got.Cause)
+				assert.EqualError(t, got.Cause, tc.expectedCause)
 			}
 		})
 	}
