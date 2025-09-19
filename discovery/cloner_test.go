154ad69d59ccc6fa619a32e127251660f0472cc6
diegogranados-pi <diego.granados@pureinsights.com>
2025-09-19 15:34:49 -0600

    PDP-3233: Fix tests

--- discovery/cloner_test.go
+++ discovery/cloner_test.go
@@ -21,9 +21,10 @@ func Test_cloner_Clone(t *testing.T) {
 		statusCode       int
 		response         string
 		expectedResponse gjson.Result
+		queryParameters  map[string][]string
 		err              error
 	}{
-		// Working case
+		// Working cases
 		{
 			name:       "Clone returns object",
 			method:     http.MethodPost,
@@ -49,10 +50,39 @@ func Test_cloner_Clone(t *testing.T) {
 			"lastUpdatedTimestamp": "2025-08-21T15:19:37.980898Z",
 			"secret": "mongo-secret"
 			}`),
-			err: nil,
+			queryParameters: map[string][]string{"name": {"mongo2"}},
+			err:             nil,
+		},
+		{
+			name:       "Clone returns object",
+			method:     http.MethodPost,
+			path:       "/5f125024-1e5e-4591-9fee-365dc20eeeed/clone",
+			statusCode: http.StatusOK,
+			response: `{
+			"type": "mongo",
+			"name": "mongo-clone",
+			"labels": [],
+			"active": true,
+			"id": "c77caced-b1d4-49de-b690-09bac3bc80a7",
+			"creationTimestamp": "2025-08-21T15:19:37.980898Z",
+			"lastUpdatedTimestamp": "2025-08-21T15:19:37.980898Z",
+			"secret": "mongo-secret"
+			}`,
+			expectedResponse: gjson.Parse(`{
+			"type": "mongo",
+			"name": "mongo-clone",
+			"labels": [],
+			"active": true,
+			"id": "c77caced-b1d4-49de-b690-09bac3bc80a7",
+			"creationTimestamp": "2025-08-21T15:19:37.980898Z",
+			"lastUpdatedTimestamp": "2025-08-21T15:19:37.980898Z",
+			"secret": "mongo-secret"
+			}`),
+			queryParameters: map[string][]string(nil),
+			err:             nil,
 		},
 
-		// Error case
+		// Error cases
 		{
 			name:             "Clone returns 404 Not Found",
 			method:           http.MethodPost,
@@ -60,6 +90,17 @@ func Test_cloner_Clone(t *testing.T) {
 			statusCode:       http.StatusNotFound,
 			response:         `{"messages": ["Entity not found: 5f125024-1e5e-4591-9fee-365dc20eeeed"]}`,
 			expectedResponse: gjson.Result{},
+			queryParameters:  map[string][]string{"name": {"mongo2"}},
+			err:              Error{Status: http.StatusNotFound, Body: gjson.Parse(`{"messages": ["Entity not found: 5f125024-1e5e-4591-9fee-365dc20eeeed"]}`)},
+		},
+		{
+			name:             "Clone returns 404 Not Found",
+			method:           http.MethodPost,
+			path:             "/5f125024-1e5e-4591-9fee-365dc20eeeed/clone",
+			statusCode:       http.StatusNotFound,
+			response:         `{"messages": ["Entity not found: 5f125024-1e5e-4591-9fee-365dc20eeeed"]}`,
+			expectedResponse: gjson.Result{},
+			queryParameters:  map[string][]string(nil),
 			err:              Error{Status: http.StatusNotFound, Body: gjson.Parse(`{"messages": ["Entity not found: 5f125024-1e5e-4591-9fee-365dc20eeeed"]}`)},
 		},
 	}
@@ -69,12 +110,15 @@ func Test_cloner_Clone(t *testing.T) {
 			srv := httptest.NewServer(testutils.HttpHandler(t, tc.statusCode, "application/json", tc.response, func(t *testing.T, r *http.Request) {
 				assert.Equal(t, tc.method, r.Method)
 				assert.Equal(t, tc.path, r.URL.Path)
+				for k, v := range tc.queryParameters {
+					assert.Equal(t, v, r.URL.Query()[k])
+				}
 			}))
 			defer srv.Close()
 
 			c := cloner{client: newClient(srv.URL, "")}
 			id := uuid.MustParse("5f125024-1e5e-4591-9fee-365dc20eeeed")
-			response, err := c.Clone(id, map[string][]string{"name": {"mongo2"}})
+			response, err := c.Clone(id, tc.queryParameters)
 			assert.Equal(t, tc.expectedResponse, response)
 			if tc.err == nil {
 				require.NoError(t, err)
