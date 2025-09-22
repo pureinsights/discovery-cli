d26bfff8d983126df5594d74852318d1bc66f351
diegogranados-pi <diego.granados@pureinsights.com>
2025-09-22 14:37:43 -0600

    PDP-3263: Reorganize functions

--- internal/cli/configuration.go
+++ internal/cli/configuration.go
@@ -1,11 +1,8 @@
 package cli
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"path/filepath"
-	"strings"
 
 	"github.com/pureinsights/pdp-cli/internal/iostreams"
 	"github.com/spf13/viper"
@@ -69,45 +66,164 @@ func InitializeConfig(ios iostreams.IOStreams, path string) (*viper.Viper, error
 	return vpr, nil
 }
 
-// SaveConfigFromUser asks the user for the URLs and API Keys of the Discovery's components to save them in a profile.
-// It then writes the current configuration into the given file.
-func (d discovery) SaveConfigFromUser(ios iostreams.IOStreams, profile string, path string) error {
-	reader := bufio.NewReader(ios.In)
-
+func (d discovery) SaveCoreConfigFromUser(profile, path string) error {
+	ios := d.IOStreams()
 	v := d.Config()
 
-	fmt.Fprintf(ios.Out, "Editing profile %q. Press Enter to keep the value shown, type a single space to set empty.\n\n", profile)
-	configKeys := []string{"core_url", "core_key", "ingestion_url", "ingestion_key", "queryflow_url", "queryflow_key", "staging_url", "staging_key"}
-	for _, k := range configKeys {
-		curr := v.Get(fmt.Sprintf("%s.%s", profile, k))
-		if curr == nil {
-			curr = "There is no value set for this property"
-		}
-		prompt := fmt.Sprintf("%s.%s [%q]: ", profile, k, curr)
-		if _, err := fmt.Fprint(ios.Out, prompt); err != nil {
-			return err
-		}
-
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return err
-		}
-
-		input := strings.TrimSuffix(strings.TrimSuffix(line, "\n"), "\r")
-
-		switch input {
-		case "":
-			continue
-		case " ":
-			v.Set(fmt.Sprintf("%s.%s", profile, k), "")
-		default:
-			v.Set(fmt.Sprintf("%s.%s", profile, k), input)
-		}
+	coreUrl, err := ios.AskUser("Core URL", v.Get(fmt.Sprintf("%s.core_url", profile)).(string))
+	if err != nil {
+		return err
 	}
 
-	err := v.WriteConfigAs(path)
+	switch coreUrl {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.core_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.core_url", profile), coreUrl)
+	}
+
+	coreKey, err := ios.AskUser("Core API Key", v.Get(fmt.Sprintf("%s.core_key", profile)).(string))
+	if err != nil {
+		return err
+	}
+
+	switch coreKey {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.core_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.core_url", profile), coreKey)
+	}
+
+	err = v.WriteConfigAs(path)
 	return err
+}
+
+func (d discovery) SaveIngestionConfigFromUser(profile, path string) error {
+	ios := d.IOStreams()
+	v := d.Config()
+
+	ingestionUrl, err := ios.AskUser("Ingestion URL", v.Get(fmt.Sprintf("%s.ingestion_url", profile)).(string))
+	if err != nil {
+		return err
+	}
+
+	switch ingestionUrl {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.ingestion_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.ingestion_url", profile), ingestionUrl)
+	}
+
+	ingestionKey, err := ios.AskUser("Ingestion API Key", v.Get(fmt.Sprintf("%s.ingestion_key", profile)).(string))
+	if err != nil {
+		return err
+	}
+
+	switch ingestionKey {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.ingestion_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.ingestion_url", profile), ingestionKey)
+	}
+
+	err = v.WriteConfigAs(path)
+	return err
+}
+
+func (d discovery) SaveQueryFlowConfigFromUser(profile, path string) error {
+	ios := d.IOStreams()
+	v := d.Config()
+
+	queryFlowUrl, err := ios.AskUser("QueryFlow URL", v.Get(fmt.Sprintf("%s.queryflow_url", profile)).(string))
+	if err != nil {
+		return err
+	}
+
+	switch queryFlowUrl {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.queryflow_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.queryflow_url", profile), queryFlowUrl)
+	}
+
+	queryFlowKey, err := ios.AskUser("QueryFlow API Key", v.Get(fmt.Sprintf("%s.queryflow_key", profile)).(string))
+	if err != nil {
+		return err
+	}
+
+	switch queryFlowKey {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.queryflow_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.queryflow_url", profile), queryFlowKey)
+	}
+
+	err = v.WriteConfigAs(path)
+	return err
+}
+
+func (d discovery) SaveStagingConfigFromUser(profile, path string) error {
+	ios := d.IOStreams()
+	v := d.Config()
+
+	stagingUrl, err := ios.AskUser("Staging URL", v.Get(fmt.Sprintf("%s.staging_url", profile)).(string))
+	if err != nil {
+		return err
+	}
+
+	switch stagingUrl {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.staging_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.staging_url", profile), stagingUrl)
+	}
+
+	stagingKey, err := ios.AskUser("Staging API Key", v.Get(fmt.Sprintf("%s.staging_key", profile)).(string))
+	if err != nil {
+		return err
+	}
+
+	switch stagingKey {
+	case "":
+
+	case " ":
+		v.Set(fmt.Sprintf("%s.staging_url", profile), "")
+	default:
+		v.Set(fmt.Sprintf("%s.staging_url", profile), stagingKey)
+	}
+
+	err = v.WriteConfigAs(path)
+	return err
+}
+
+// SaveConfigFromUser asks the user for the URLs and API Keys of the Discovery's components to save them in a profile.
+// It then writes the current configuration into the given file.
+func (d discovery) SaveConfigFromUser(profile string, path string) error {
+	err := d.SaveCoreConfigFromUser(profile, path)
+	if err != nil {
+		return err
+	}
+	err = d.SaveIngestionConfigFromUser(profile, path)
+	if err != nil {
+		return err
+	}
+	err = d.SaveQueryFlowConfigFromUser(profile, path)
+	if err != nil {
+		return err
+	}
+	return d.SaveStagingConfigFromUser(profile, path)
 }
--- internal/iostreams/iostreams.go
+++ internal/iostreams/iostreams.go
@@ -1,10 +1,32 @@
 package iostreams
 
-import "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
 
 // IOStreams is the struct contains the streams to read from the standard input and write to the standard output and error.
 type IOStreams struct {
 	In  io.Reader
 	Out io.Writer
 	Err io.Writer
+}
+
+func (ios IOStreams) AskUser(question, defaultValue string) (string, error) {
+	reader := bufio.NewReader(ios.In)
+
+	prompt := fmt.Sprintf("%s [%q]: ", question, defaultValue)
+	if _, err := fmt.Fprint(ios.Out, prompt); err != nil {
+		return "", err
+	}
+
+	line, err := reader.ReadString('\n')
+	if err != nil {
+
+		return "", err
+	}
+
+	return strings.TrimSuffix(strings.TrimSuffix(line, "\n"), "\r"), nil
 }
