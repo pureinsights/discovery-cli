3f346c08ec1119641e40c02a6715f962e7b8dc8d
diegogranados-pi <diego.granados@pureinsights.com>
2025-09-29 14:15:32 -0600

    PDP-3234: searcher

--- discovery/searcher.go
+++ discovery/searcher.go
@@ -10,32 +10,6 @@ type searcher struct {
 	client
 }
 
-func (c cloner) Search(filter gjson.Result, params map[string][]string) ([]gjson.Result, error) {
-	response, err := execute(c.client, http.MethodPost, "/search", WithQueryParameters(params))
-	if err == nil {
-		elementNumber := response.Get("numberOfElements").Int()
-		pageNumber := response.Get("pageNumber").Int()
-		totalPages := response.Get("totalPages").Int()
-		totalSize := response.Get("totalSize").Int()
-		elements := response.Get("content").Array()
-		for pageNumber < totalPages || elementNumber < totalSize {
-			response, err = execute(c.client, http.MethodPost, "/search", WithQueryParameters(params))
-			if err == nil {
-				pageElementNumber := response.Get("numberOfElements").Int()
-				pagePageNumber := response.Get("pageNumber").Int()
-				pageElements := response.Get("content").Array()
-			} else {
-
-				return []gjson.Result{}, err
-			}
-		}
-		return response, nil
-	} else {
-
-		return []gjson.Result{}, err
-	}
-}
-
-func Autocomplete(q string) ([]gjson.Result, error) {
-
+func (s searcher) Search(filter gjson.Result) ([]gjson.Result, error) {
+	return executeWithPagination(s.client, http.MethodPost, "/search", WithJSONBody(filter.Raw))
 }
--- main.go
+++ main.go
@@ -1,3 +1,7 @@
 package main
 
-func main() {}
+import "github.com/pureinsights/pdp-cli/discovery"
+
+func main() {
+	discovery.Manual()
+}
