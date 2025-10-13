f7d61b996df71221971534ec468be34ecac1011e
diegogranados-pi <diego.granados@pureinsights.com>
2025-10-13 17:53:17 -0600

    PDP-3302: Add functions

--- internal/cli/entity.go
+++ internal/cli/entity.go
@@ -5,15 +5,42 @@ import (
 	"github.com/tidwall/gjson"
 )
 
+// Getter defines the Get and GetAll methods.
 type getter interface {
 	Get(uuid.UUID) (gjson.Result, error)
 	GetAll() ([]gjson.Result, error)
 }
 
+// GetEntity obtains the entity with the given ID using the given client and then prints out the result using the received printer or the JSON printer.
 func (d discovery) GetEntity(client getter, id uuid.UUID, printer Printer) error {
 	object, err := client.Get(id)
 	if err != nil {
 		return NewErrorWithCause(ErrorExitCode, err, "Could not get entity with id %q", id.String())
 	}
 
+	if printer == nil {
+		jsonPrinter := JsonObjectPrinter(false)
+		err = jsonPrinter(*d.IOStreams(), object)
+	} else {
+		err = printer(*d.IOStreams(), object)
+	}
+
+	return err
+}
+
+// GetEntities obtains all of the entities using the given client and then prints out the result using the received printer or the JSON array printer.
+func (d discovery) GetEntities(client getter, printer Printer) error {
+	objects, err := client.GetAll()
+	if err != nil {
+		return NewErrorWithCause(ErrorExitCode, err, "Could not get all entities")
+	}
+
+	if printer == nil {
+		arrayPrinter := JsonArrayPrinter(false)
+		err = arrayPrinter(*d.IOStreams(), objects...)
+	} else {
+		err = printer(*d.IOStreams(), objects...)
+	}
+
+	return err
 }
---
+++ internal/cli/entity_test.go
@@ -0,0 +1,13 @@
+package cli
+
+import "testing"
+
+// GetEntity obtains the entity with the given ID using the given client and then prints out the result using the received printer or the JSON printer.
+func Test_discovery_GetEntity(t *testing.T) {
+
+}
+
+// GetEntities obtains all of the entities using the given client and then prints out the result using the received printer or the JSON array printer.
+func Test_discovery_GetEntities(t *testing.T) error {
+
+}
