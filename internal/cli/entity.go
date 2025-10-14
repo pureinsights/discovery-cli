9060699c44657cc7a448e920d05a5a38d88c0fec
diegogranados-pi <diego.granados@pureinsights.com>
2025-10-14 10:33:41 -0600

    PDP-3305: Start on search entity functions

--- internal/cli/entity.go
+++ internal/cli/entity.go
@@ -1,8 +1,12 @@
 package cli
 
 import (
+	"net/http"
+
 	"github.com/google/uuid"
 	"github.com/tidwall/gjson"
+
+	discoveryPackage "github.com/pureinsights/pdp-cli/discovery"
 )
 
 // Getter defines the Get and GetAll methods.
@@ -44,3 +48,70 @@ func (d discovery) GetEntities(client getter, printer Printer) error {
 
 	return err
 }
+
+type searcher interface {
+	getter
+	Search(gjson.Result) ([]gjson.Result, error)
+	SearchByName(name string) (gjson.Result, error)
+}
+
+func (d discovery) searchEntity(client searcher, id string) (gjson.Result, error) {
+	result, err := client.SearchByName(id)
+	if err != nil {
+		discoveryErr, ok := err.(discoveryPackage.Error)
+		if !ok {
+			return gjson.Result{}, err
+		}
+
+		if discoveryErr.Status != http.StatusNotFound {
+			return gjson.Result{}, discoveryErr
+		}
+
+		if parsedId, uuidErr := uuid.Parse(id); uuidErr == nil {
+			result, err = client.Get(parsedId)
+			if err != nil {
+				return gjson.Result{}, err
+			}
+
+			return result, nil
+		}
+
+		return gjson.Result{}, discoveryErr
+	}
+
+	return result, nil
+}
+
+func (d discovery) SearchEntity(client searcher, id string, printer Printer) error {
+	result, err := d.searchEntity(client, id)
+	if err != nil {
+		return err
+	}
+
+	if printer == nil {
+		jsonPrinter := JsonObjectPrinter(false)
+		err = jsonPrinter(*d.IOStreams(), result)
+	} else {
+		err = printer(*d.IOStreams(), result)
+	}
+
+	return err
+}
+
+func (d discovery) SearchEntities(client searcher, filter gjson.Result, printer Printer) error {
+	results, err := client.Search(filter)
+	if err != nil {
+		return err
+	}
+
+	if printer == nil {
+		jsonPrinter := JsonArrayPrinter(false)
+		err = jsonPrinter(*d.IOStreams(), results...)
+	} else {
+		err = printer(*d.IOStreams(), results...)
+	}
+
+	return err
+}
+
+func BuildEntitiesFilter(filters []string) (gjson.Result, error)
