dcdb84a3134bbb7ab9168afab114cf9b64c37391
diegogranados-pi <diego.granados@pureinsights.com>
2025-10-16 17:10:50 -0600

    PDP-3304: Fix unit test

--- cmd/core/core_test.go
+++ cmd/core/core_test.go
@@ -47,7 +47,7 @@ func Test_NewCoreCommand(t *testing.T) {
 		commandNames = append(commandNames, c.Name())
 	}
 
-	expectedCommands := []string{"config", "secrets"}
+	expectedCommands := []string{"config", "label", "secret"}
 	for _, c := range expectedCommands {
 		require.Contains(t, commandNames, c)
 	}
