eb888360c4e63ae6354f5e2dcc2458a94692a448
diegogranados-pi <diego.granados@pureinsights.com>
2025-10-24 18:00:46 -0600

    PDP-3321: Add server delete command

---
+++ cmd/core/servers/delete.go
@@ -0,0 +1,31 @@
+package servers
+
+import (
+	"fmt"
+
+	discoveryPackage "github.com/pureinsights/pdp-cli/discovery"
+	"github.com/pureinsights/pdp-cli/internal/cli"
+	"github.com/spf13/cobra"
+)
+
+// NewDeleteCommand creates the server delete command
+func NewDeleteCommand(d cli.Discovery) *cobra.Command {
+	get := &cobra.Command{
+		Use:   "delete",
+		Short: "The command that deletes servers from Discovery Core.",
+		Long:  fmt.Sprintf(cli.LongDeleteNoNames, "server", "Core"),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			profile, err := cmd.Flags().GetString("profile")
+			if err != nil {
+				return cli.NewErrorWithCause(cli.ErrorExitCode, err, "Could not get the profile")
+			}
+
+			vpr := d.Config()
+
+			coreClient := discoveryPackage.NewCore(vpr.GetString(profile+".core_url"), vpr.GetString(profile+".core_key"))
+			return cli.SearchDeleteCommand(args[0], d, coreClient.Servers(), cli.GetCommandConfig(profile, vpr.GetString("output"), "Core", "core_url", "core_key"))
+		},
+		Args: cobra.ExactArgs(1),
+	}
+	return get
+}
