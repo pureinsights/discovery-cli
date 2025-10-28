3afe96c0d034499a1f8c8a80075b56ba13e6e3de
diegogranados-pi <diego.granados@pureinsights.com>
2025-10-27 20:02:09 -0600

    PDP-3305: Fix tests

--- internal/cli/entity_test.go
+++ internal/cli/entity_test.go
@@ -792,7 +792,7 @@ func Test_discovery_SearchEntities(t *testing.T) {
 			name:           "SearchEntities correctly prints an array with the sent printer",
 			client:         new(WorkingSearcher),
 			printer:        JsonArrayPrinter(true),
-			expectedOutput: "[\n{\n  \"highlight\": {},\n  \"score\": 0.20970252,\n  \"source\": {\n    \"active\": true,\n    \"creationTimestamp\": \"2025-09-29T15:50:17Z\",\n    \"id\": \"986ce864-af76-4fcb-8b4f-f4e4c6ab0951\",\n    \"labels\": [],\n    \"lastUpdatedTimestamp\": \"2025-09-29T15:50:17Z\",\n    \"name\": \"MongoDB Atlas server clone\",\n    \"type\": \"mongo\"\n  }\n}\n{\n  \"highlight\": {},\n  \"score\": 0.20970252,\n  \"source\": {\n    \"active\": true,\n    \"creationTimestamp\": \"2025-09-29T15:50:19Z\",\n    \"id\": \"8f14c11c-bb66-49d3-aa2a-dedff4608c17\",\n    \"labels\": [],\n    \"lastUpdatedTimestamp\": \"2025-09-29T15:50:19Z\",\n    \"name\": \"MongoDB Atlas server clone 1\",\n    \"type\": \"mongo\"\n  }\n}\n{\n  \"highlight\": {},\n  \"score\": 0.20970252,\n  \"source\": {\n    \"active\": true,\n    \"creationTimestamp\": \"2025-09-29T15:50:20Z\",\n    \"id\": \"3a0214a4-72cc-4eee-ad0c-9e3af9b08a6c\",\n    \"labels\": [],\n    \"lastUpdatedTimestamp\": \"2025-09-29T15:50:20Z\",\n    \"name\": \"MongoDB Atlas server clone 3\",\n    \"type\": \"mongo\"\n  }\n}\n]\n",
+			expectedOutput: "[\n{\n  \"highlight\": {},\n  \"score\": 0.20970252,\n  \"source\": {\n    \"active\": true,\n    \"creationTimestamp\": \"2025-09-29T15:50:17Z\",\n    \"id\": \"986ce864-af76-4fcb-8b4f-f4e4c6ab0951\",\n    \"labels\": [],\n    \"lastUpdatedTimestamp\": \"2025-09-29T15:50:17Z\",\n    \"name\": \"MongoDB Atlas server clone\",\n    \"type\": \"mongo\"\n  }\n},\n{\n  \"highlight\": {},\n  \"score\": 0.20970252,\n  \"source\": {\n    \"active\": true,\n    \"creationTimestamp\": \"2025-09-29T15:50:19Z\",\n    \"id\": \"8f14c11c-bb66-49d3-aa2a-dedff4608c17\",\n    \"labels\": [],\n    \"lastUpdatedTimestamp\": \"2025-09-29T15:50:19Z\",\n    \"name\": \"MongoDB Atlas server clone 1\",\n    \"type\": \"mongo\"\n  }\n},\n{\n  \"highlight\": {},\n  \"score\": 0.20970252,\n  \"source\": {\n    \"active\": true,\n    \"creationTimestamp\": \"2025-09-29T15:50:20Z\",\n    \"id\": \"3a0214a4-72cc-4eee-ad0c-9e3af9b08a6c\",\n    \"labels\": [],\n    \"lastUpdatedTimestamp\": \"2025-09-29T15:50:20Z\",\n    \"name\": \"MongoDB Atlas server clone 3\",\n    \"type\": \"mongo\"\n  }\n}\n]\n",
 			err:            nil,
 		},
 		{
@@ -816,7 +816,7 @@ func Test_discovery_SearchEntities(t *testing.T) {
 			client:    new(WorkingSearcher),
 			printer:   nil,
 			outWriter: testutils.ErrWriter{Err: errors.New("write failed")},
-			err:       NewErrorWithCause(ErrorExitCode, errors.New("write failed"), "Could not print JSON array"),
+			err:       NewErrorWithCause(ErrorExitCode, errors.New("write failed"), "Could not print JSON Array"),
 		},
 	}
 
