fa257270d5c2764e17e54c29e400bedd351c2c3f
diegogranados-pi <diego.granados@pureinsights.com>
2025-10-28 11:22:59 -0600

    PDP-3320: Add search delete command unit tests

--- internal/cli/component_delete_test.go
+++ internal/cli/component_delete_test.go
@@ -136,3 +136,113 @@ func TestDeleteCommand(t *testing.T) {
 		})
 	}
 }
+
+// TestSearchDeleteCommand tests the SearchDeleteCommand() function
+func TestSearchDeleteCommand(t *testing.T) {
+	tests := []struct {
+		name           string
+		client         searchDeleter
+		args           string
+		url            string
+		apiKey         string
+		componentName  string
+		expectedOutput string
+		outWriter      io.Writer
+		err            error
+	}{
+		// Working case
+		{
+			name:           "SearchDeleteEntity correctly prints an object",
+			url:            "http://localhost:12010/v2",
+			apiKey:         "core123",
+			componentName:  "Core",
+			args:           "MongoDB Atlas Server",
+			client:         new(WorkingSearchDeleter),
+			expectedOutput: "{\"acknowledged\":true}\n",
+			err:            nil,
+		},
+
+		// Error case
+		{
+			name:          "CheckCredentials fails",
+			client:        new(WorkingSearchDeleter),
+			url:           "",
+			apiKey:        "core123",
+			componentName: "Core",
+			args:          "",
+			outWriter:     testutils.ErrWriter{Err: errors.New("write failed")},
+			err:           NewError(ErrorExitCode, "The Discovery Core URL is missing for profile \"default\".\nTo set the URL for the Discovery Core API, run any of the following commands:\n      discovery config  --profile {profile}\n      discovery core config --profile {profile}"),
+		},
+		{
+			name:           "SearchDeleteEntity returns 400 Bad Request",
+			client:         new(FailingSearchDeleterSearchFails),
+			url:            "http://localhost:12010/v2",
+			apiKey:         "core123",
+			componentName:  "Core",
+			args:           "MongoDB Atlas Server",
+			expectedOutput: "",
+			err: NewErrorWithCause(ErrorExitCode, discoveryPackage.Error{
+				Status: http.StatusNotFound,
+				Body: gjson.Parse(`{
+	"status": 404,
+	"code": 1003,
+	"messages": [
+		"Entity not found: entity with name "MongoDB Atlas Server" does not exist"
+	],
+	"timestamp": "2025-09-30T15:38:42.885125200Z"
+}`),
+			}, "Could not search for entity with name \"MongoDB Atlas Server\""),
+		},
+		{
+			name:          "Printing JSON fails",
+			client:        new(WorkingSearchDeleter),
+			url:           "http://localhost:12010/v2",
+			apiKey:        "core123",
+			componentName: "Core",
+			args:          "MongoDB Atlas Server",
+			outWriter:     testutils.ErrWriter{Err: errors.New("write failed")},
+			err:           NewErrorWithCause(ErrorExitCode, errors.New("write failed"), "Could not print JSON object"),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			var out io.Writer
+			if tc.outWriter != nil {
+				out = tc.outWriter
+			} else {
+				out = buf
+			}
+
+			ios := iostreams.IOStreams{
+				In:  os.Stdin,
+				Out: out,
+				Err: os.Stderr,
+			}
+
+			vpr := viper.New()
+			vpr.Set("profile", "default")
+			vpr.Set("output", "json")
+			if tc.url != "" {
+				vpr.Set("default.core_url", tc.url)
+			}
+			if tc.apiKey != "" {
+				vpr.Set("default.core_key", tc.apiKey)
+			}
+
+			d := NewDiscovery(&ios, vpr, "")
+			err := SearchDeleteCommand(tc.args, d, tc.client, GetCommandConfig("default", "json", tc.componentName, "core_url", "core_key"))
+
+			if tc.err != nil {
+				require.Error(t, err)
+				var errStruct Error
+				require.ErrorAs(t, err, &errStruct)
+				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedOutput, buf.String())
+			}
+		})
+	}
+}
