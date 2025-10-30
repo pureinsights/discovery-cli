344009231fa7adc7fa98ca48f3a23c901533cf24
diegogranados-pi <diego.granados@pureinsights.com>
2025-10-30 14:19:09 -0600

    PDP-3331: Add ingestion processor store, missing docs

---
+++ cmd/ingestion/processors/store.go
@@ -0,0 +1,41 @@
+package processors
+
+import (
+	"fmt"
+
+	"github.com/pureinsights/pdp-cli/cmd/commands"
+	discoveryPackage "github.com/pureinsights/pdp-cli/discovery"
+	"github.com/pureinsights/pdp-cli/internal/cli"
+	"github.com/spf13/cobra"
+)
+
+// NewStoreCommand creates the processor store command
+func NewStoreCommand(d cli.Discovery) *cobra.Command {
+	var abortOnError bool
+	var data string
+	var file string
+	store := &cobra.Command{
+		Use:   "store",
+		Short: "The command that stores processors to Discovery Ingestion.",
+		Long:  fmt.Sprintf(commands.LongStore, "processor", "Ingestion"),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			profile, err := cmd.Flags().GetString("profile")
+			if err != nil {
+				return cli.NewErrorWithCause(cli.ErrorExitCode, err, "Could not get the profile")
+			}
+
+			vpr := d.Config()
+
+			ingestionClient := discoveryPackage.NewIngestion(vpr.GetString(profile+".ingestion_url"), vpr.GetString(profile+".ingestion_key"))
+			return commands.StoreCommand(d, ingestionClient.Processors(), commands.StoreCommandConfig(commands.GetCommandConfig(profile, vpr.GetString("output"), "Ingestion", "ingestion_url", "ingestion_key"), abortOnError, data, file))
+		},
+		Args: cobra.MaximumNArgs(1),
+	}
+	store.Flags().BoolVar(&abortOnError, "abort-on-error", false, "Aborts the operation if there is an error")
+	store.Flags().StringVarP(&data, "data", "d", "", "The JSON with the configurations that will be upserted.")
+	store.Flags().StringVarP(&file, "file", "f", "", "The address of the file that contains the JSON data")
+
+	store.MarkFlagsOneRequired("data", "file")
+	store.MarkFlagsMutuallyExclusive("data", "file")
+	return store
+}
