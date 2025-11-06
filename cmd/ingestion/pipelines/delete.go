9b158711de93ea726e0ee002baaa96a2cd58967f
diegogranados-pi <diego.granados@pureinsights.com>
2025-11-06 14:42:28 -0600

    PDP-3348: Add ingestion pipeline delete

---
+++ cmd/ingestion/pipelines/delete.go
@@ -0,0 +1,32 @@
+package pipelines
+
+import (
+	"fmt"
+
+	"github.com/pureinsights/pdp-cli/cmd/commands"
+	discoveryPackage "github.com/pureinsights/pdp-cli/discovery"
+	"github.com/pureinsights/pdp-cli/internal/cli"
+	"github.com/spf13/cobra"
+)
+
+// NewDeleteCommand creates the pipeline delete command
+func NewDeleteCommand(d cli.Discovery) *cobra.Command {
+	get := &cobra.Command{
+		Use:   "delete",
+		Short: "The command that deletes pipelines from Discovery Ingestion.",
+		Long:  fmt.Sprintf(commands.LongDeleteNoNames, "pipeline", "Ingestion"),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			profile, err := cmd.Flags().GetString("profile")
+			if err != nil {
+				return cli.NewErrorWithCause(cli.ErrorExitCode, err, "Could not get the profile")
+			}
+
+			vpr := d.Config()
+
+			ingestionClient := discoveryPackage.NewIngestion(vpr.GetString(profile+".ingestion_url"), vpr.GetString(profile+".ingestion_key"))
+			return commands.SearchDeleteCommand(args[0], d, ingestionClient.Pipelines(), commands.GetCommandConfig(profile, vpr.GetString("output"), "Ingestion", "ingestion_url"))
+		},
+		Args: cobra.ExactArgs(1),
+	}
+	return get
+}
