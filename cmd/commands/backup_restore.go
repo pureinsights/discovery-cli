464d80f2e8f75ef8044fd3346b5604808b295873
diegogranados-pi <diego.granados@pureinsights.com>
2025-11-12 14:18:40 -0600

    PDP-3363: Add export command and tests

---
+++ cmd/commands/backup_restore.go
@@ -0,0 +1,20 @@
+package commands
+
+import "github.com/pureinsights/pdp-cli/internal/cli"
+
+const (
+	// LongExport is the message used in the Long field of the Export commands.
+	LongExport string = "export is the command used to backup Discovery %[2]s's entities. The user can send the specific file in which to save the configurations. If not, they will be saved in a zip file in the current directory."
+)
+
+// ExportCommand is the function that executes the export operation
+func ExportCommand(d cli.Discovery, client cli.BackupRestore, file string, config commandConfig) error {
+	err := CheckCredentials(d, config.profile, config.componentName, config.url)
+	if err != nil {
+		return err
+	}
+
+	printer := cli.GetObjectPrinter(d.Config().GetString("output"))
+
+	return d.ExportEntitiesFromClient(client, file, printer)
+}
