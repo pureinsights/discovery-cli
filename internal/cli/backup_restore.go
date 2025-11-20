55fbd5c1acaf25377f9e6feedd4b291c2bd38529
diegogranados-pi <diego.granados@pureinsights.com>
2025-11-20 16:33:42 -0600

    PDP-3373: Create the ImportEntitiesToClients methods (#169)
    * PDP-3368: Add the modified backup restore
    
    * PDP-3362: Add backup restore tests
    
    * PDP-3362: Add docs to test
    
    * PDP-3369: Start on Export clients
    
    * PDP-3369: Start on tests
    
    * PDP-3369: Add unit tests of exportentitiesfromclients
    
    * PDP-3363: Add export command and tests
    
    * PDP-3362: Remove extra space
    
    * PDP-3363: Remove extra spacing
    
    * PDP-3364: Add core export command
    
    * PDP-3364: Improve docs
    
    * PDP-3364: Add ingestion export command
    
    * PDP-3364: Remove println
    
    * PDP-3364: Remove coverage file
    
    * PDP-3366: Add queryflow export command
    
    * PDP-3365: Fix ingestion export
    
    * PDP-3367: Add Discovery Export command
    
    * PDP-3367: Fix code smells
    
    * PDP-3364: Rename command file to export
    
    * PDP-3367: Fix export comments
    
    * PDP-3364: Fix comments
    
    * PDP-3372: Start on import entities
    
    * PDP-3363: Remove typo
    
    * PDP-3372: Finish ImportEntitiesFromClient
    
    * PDP-3373: Finish unit tests
    
    * PDP-3373: Fix typos
    
    * PDP-3363: Fix failing tests
    
    * PDP-3367: Fix docs
    
    * PDP-3367: add docs to struct in tests
    
    * PDP-3362: Address pr comments
    
    * PDP-3369: Fix tests
    
    * PDP-3369: Address PR comments
    
    * PDP-3363: Create the common ExportCommand function (#163)
    
    * PDP-3363: Add export command and tests
    
    * PDP-3363: Remove extra spacing
    
    * PDP-3363: Remove typo
    
    * PDP-3363: Fix failing tests
    
    * PDP-3365: Create the Ingestion Export command (#165)
    
    * PDP-3364: Add ingestion export command
    
    * PDP-3365: Fix ingestion export
    
    * PDP-3366: Create QueryFlow Export command (#166)
    
    * PDP-3364: Add ingestion export command
    
    * PDP-3366: Add queryflow export command
    
    * PDP-3365: Fix ingestion export
    
    * PDP-3367: Create the Discovery export command (#167)
    
    * PDP-3364: Add ingestion export command
    
    * PDP-3366: Add queryflow export command
    
    * PDP-3365: Fix ingestion export
    
    * PDP-3367: Add Discovery Export command
    
    * PDP-3367: Fix code smells
    
    * PDP-3367: Fix export comments
    
    * PDP-3367: Fix docs
    
    * PDP-3367: add docs to struct in tests
    
    * PDP-3372: Create the ImportEntitiesToClient method (#168)
    
    * PDP-3364: Add ingestion export command
    
    * PDP-3366: Add queryflow export command
    
    * PDP-3365: Fix ingestion export
    
    * PDP-3367: Add Discovery Export command
    
    * PDP-3367: Fix code smells
    
    * PDP-3367: Fix export comments
    
    * PDP-3372: Start on import entities
    
    * PDP-3372: Finish ImportEntitiesFromClient
    
    * PDP-3367: Fix docs
    
    * PDP-3367: add docs to struct in tests
    
    * PDP-3373: Try to fix code smells
    
    * PDP-3364: Revert change
    
    * PDP-3373: Fix code smell
    
    * PDP-3373: Revert coverage

--- internal/cli/backup_restore.go
+++ internal/cli/backup_restore.go
@@ -2,9 +2,12 @@ package cli
 
 import (
 	"archive/zip"
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
@@ -42,7 +45,7 @@ func WriteExport(client BackupRestore, path string) (gjson.Result, error) {
 	return RenderExportStatus(err)
 }
 
-// ExportEntitiesFromClient exports the entities from a single Discovery Component and prints the acknowledgement message
+// ExportEntitiesFromClient exports the entities from a single Discovery product and prints the acknowledgement message
 func (d discovery) ExportEntitiesFromClient(client BackupRestore, path string, printer Printer) error {
 	result, err := WriteExport(client, path)
 	if err != nil {
@@ -56,7 +59,7 @@ func (d discovery) ExportEntitiesFromClient(client BackupRestore, path string, p
 	return printer(*d.iostreams, result)
 }
 
-// BackupRestoreClientEntry is used to easily store the different backup and restore structs of the Discovery components.
+// BackupRestoreClientEntry is used to easily store the different backup and restore structs of the Discovery products.
 type BackupRestoreClientEntry struct {
 	Name   string
 	Client BackupRestore
@@ -137,7 +140,7 @@ func (d discovery) ExportEntitiesFromClients(clients []BackupRestoreClientEntry,
 	return printer(*d.iostreams, gjson.Parse(result))
 }
 
-// ImportEntitiesToClient imports the entities to a Discovery Component by reading them from the given path and using the given conflict resolution strategy.
+// ImportEntitiesToClient imports the entities to a Discovery product by reading them from the given path and using the given conflict resolution strategy.
 // It then prints out the results.
 func (d discovery) ImportEntitiesToClient(client BackupRestore, path string, onConflict discoveryPackage.OnConflict, printer Printer) error {
 	results, err := client.Import(onConflict, path)
@@ -150,4 +153,132 @@ func (d discovery) ImportEntitiesToClient(client BackupRestore, path string, onC
 	}
 
 	return printer(*d.iostreams, results)
+}
+
+// copyImportEntitiesToTempFile copies the information of the inner zip file to the temporary file to be used in the import endpoint call.
+func copyImportEntitiesToTempFile(file *zip.File, path string) error {
+	if file.FileInfo().IsDir() {
+		return NewError(ErrorExitCode, "The sent file should only contain the Core, Ingestion, or QueryFlow export files.")
+	}
+
+	readCloser, err := file.Open()
+	if err != nil {
+		return NewErrorWithCause(ErrorExitCode, err, "Could not open a file contained within the zip")
+	}
+	defer readCloser.Close()
+
+	out, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, file.Mode())
+	if err != nil {
+		return NewErrorWithCause(ErrorExitCode, err, "Could not create the temporary export file")
+	}
+	defer out.Close()
+
+	if _, err := io.Copy(out, readCloser); err != nil {
+		return NewErrorWithCause(ErrorExitCode, err, "Could not copy the file's contents")
+	}
+
+	return nil
+}
+
+// readInnerZipFiles reads the inner zip files that contain the entities to be imported.
+// It writes the files into a temporary directory.
+func readInnerZipFiles(tmpDir string, zipReader *zip.Reader) (map[string]string, error) {
+	paths := map[string]string{}
+	expectedPrefixes := []string{"core", "ingestion", "queryflow"}
+
+	for _, file := range zipReader.File {
+		// Validate zip slip vulnerability
+		destPath := filepath.Join(tmpDir, file.Name)
+		if !strings.HasPrefix(filepath.Clean(destPath)+string(os.PathSeparator),
+			filepath.Clean(tmpDir)+string(os.PathSeparator)) {
+			return nil, NewError(ErrorExitCode, "The sent file contains malicious entries.")
+		}
+
+		err := copyImportEntitiesToTempFile(file, destPath)
+		if err != nil {
+			return nil, err
+		}
+
+		base := filepath.Base(file.Name)
+		for _, prefix := range expectedPrefixes {
+			if strings.HasPrefix(base, prefix) {
+				paths[prefix] = destPath
+			}
+		}
+	}
+	return paths, nil
+}
+
+// UnzipExportsToTemp parses the files read from the discovery export file.
+func UnzipExportsToTemp(zipBytes []byte) (string, map[string]string, error) {
+	zipReader, err := zip.NewReader(bytes.NewReader(zipBytes), int64(len(zipBytes)))
+	if err != nil {
+		return "", nil, NewErrorWithCause(ErrorExitCode, err, "Could not read the file with the entities")
+	}
+
+	if len(zipReader.File) > 3 {
+		return "", nil, NewError(ErrorExitCode, "The sent file should only contain the Core, Ingestion, or QueryFlow export files.")
+	}
+
+	tmpDir, err := os.MkdirTemp("", "discovery-import-*")
+	if err != nil {
+		return "", nil, NewErrorWithCause(ErrorExitCode, err, "Could not create temporary directory to import entities")
+	}
+
+	paths, err := readInnerZipFiles(tmpDir, zipReader)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return tmpDir, paths, nil
+}
+
+// CallImports calls the import endpoints of the given clients and adds the responses to the results JSON.
+func callImports(clients []BackupRestoreClientEntry, zipPaths map[string]string, onConflict discoveryPackage.OnConflict) (string, error) {
+	results := "{}"
+	for _, client := range clients {
+		if path, ok := zipPaths[client.Name]; ok {
+			importResult, err := client.Client.Import(onConflict, path)
+			if err == nil {
+				results, err = sjson.SetRaw(results, client.Name, importResult.Raw)
+				if err != nil {
+					return "", NewErrorWithCause(ErrorExitCode, err, "Could not write import entities")
+				}
+			} else {
+				results, err = sjson.Set(results, client.Name, err.Error())
+				if err != nil {
+					return "", NewErrorWithCause(ErrorExitCode, err, "Could not write import entities")
+				}
+			}
+		}
+	}
+
+	return results, nil
+}
+
+// ImportEntitiesToClients reads a zip file that contains the zip files of exported entities. The files must have the name of the Discovery product in which the entities are going to be restored.
+// The given zip file does not need to have the export files for Core, Ingestion, and QueryFlow. It can have some of them.
+// The results of the imports are then printed out in a JSON to the user.
+func (d discovery) ImportEntitiesToClients(clients []BackupRestoreClientEntry, path string, onConflict discoveryPackage.OnConflict, printer Printer) error {
+	zipFile, err := os.ReadFile(path)
+	if err != nil {
+		return NewErrorWithCause(ErrorExitCode, err, "Could not open the file with the entities")
+	}
+
+	tmpDir, zipPaths, err := UnzipExportsToTemp(zipFile)
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmpDir)
+
+	results, err := callImports(clients, zipPaths, onConflict)
+	if err != nil {
+		return err
+	}
+
+	if printer == nil {
+		printer = JsonObjectPrinter(false)
+	}
+
+	return printer(*d.iostreams, gjson.Parse(results))
 }
