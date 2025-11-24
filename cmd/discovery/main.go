a124eec1da5861331838595b8e72031aa02b6efa
diegogranados-pi <diego.granados@pureinsights.com>
2025-11-24 17:13:04 -0600

    PDP-3378: Fix main

--- cmd/discovery/main.go
+++ cmd/discovery/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"slices"
 
 	"github.com/pureinsights/pdp-cli/cmd"
+	"github.com/pureinsights/pdp-cli/internal/cli"
 )
 
 func main() {
 	exitCode, err := cmd.Run()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "An error occurred: %s", err)
+		var cliError *cli.Error
+		if errors.As(err, &cliError) && slices.Contains([]string{"Could not access the user's Home directory", "Could not create the /.discovery directory", "Could not read the configuration file", "Could not read the credentials file"}, cliError.Message) {
+			fmt.Fprintf(os.Stderr, "An error occurred: %s", err)
+		}
 	}
 	os.Exit(int(exitCode))
 }
