5844f04dca7d85824a012e17744125ce9ed1c881
diegogranados-pi <diego.granados@pureinsights.com>
2025-11-27 10:24:11 -0600

    PDP-3342: try to fix code smell

--- cmd/ingestion/seeds/halt.go
+++ cmd/ingestion/seeds/halt.go
@@ -14,7 +14,7 @@ func NewHaltCommand(d cli.Discovery) *cobra.Command {
 	halt := &cobra.Command{
 		Use:   "halt",
 		Short: "The command that halts a seed execution in Discovery Ingestion.",
-		Long:  "halt is the command used to halt a seed execution in Discovery Ingestion. With the execution flag, the user can specify the specific execution that will be halted. If there is no execution flag, all of the active executions are halted.",
+		Long:  "halt is the command used to halt a seed execution in Discovery Ingestion. With the --execution flag, the user can specify the specific execution that will be halted. If there is no --execution flag, all of the active executions are halted.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			profile, err := cmd.Flags().GetString("profile")
 			if err != nil {
@@ -32,16 +32,16 @@ func NewHaltCommand(d cli.Discovery) *cobra.Command {
 			printer := cli.GetObjectPrinter(vpr.GetString("output"))
 			if execution == "" {
 				return d.HaltSeed(ingestionClient.Seeds(), args[0], printer)
-			} else {
-				if executionId, err := uuid.Parse(execution); err == nil {
-					seedsClient := ingestionClient.Seeds()
-					if seedId, err := cli.GetSeedId(d, seedsClient, args[0]); err == nil {
-						return d.HaltSeedExecution(seedsClient.Executions(seedId), executionId, printer)
-					}
-				}
-
-				return cli.NewErrorWithCause(cli.ErrorExitCode, err, "Failed to convert the execution ID to UUID")
 			}
+
+			if executionId, err := uuid.Parse(execution); err == nil {
+				seedsClient := ingestionClient.Seeds()
+				if seedId, err := cli.GetSeedId(d, seedsClient, args[0]); err == nil {
+					return d.HaltSeedExecution(seedsClient.Executions(seedId), executionId, printer)
+				}
+			}
+
+			return cli.NewErrorWithCause(cli.ErrorExitCode, err, "Failed to convert the execution ID to UUID")
 		},
 		Args: cobra.ExactArgs(1),
 		Example: `	# Halt all active seed executions
