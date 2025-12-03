ce097c8a3f97df9b5b541ccd6ed18ba315b9423e
diegogranados-pi <diego.granados@pureinsights.com>
2025-12-03 16:40:57 -0600

    PDP-3388: Change to develop-cli

--- cmd/backuprestore/export.go
+++ cmd/backuprestore/export.go
@@ -1,9 +1,9 @@
 package backuprestore
 
 import (
-	"github.com/pureinsights/pdp-cli/cmd/commands"
-	discoveryPackage "github.com/pureinsights/pdp-cli/discovery"
-	"github.com/pureinsights/pdp-cli/internal/cli"
+	"github.com/pureinsights/discovery-cli/cmd/commands"
+	discoveryPackage "github.com/pureinsights/discovery-cli/discovery"
+	"github.com/pureinsights/discovery-cli/internal/cli"
 	"github.com/spf13/cobra"
 )
 
