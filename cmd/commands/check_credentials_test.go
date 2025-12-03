f4a9e8b7ea48ea336ec525fce4199753f7fd7859
diegogranados-pi <diego.granados@pureinsights.com>
2025-12-03 16:11:18 -0600

    PDP-3359: Revert weird change

--- cmd/commands/check_credentials_test.go
+++ cmd/commands/check_credentials_test.go
@@ -44,7 +44,7 @@ func TestCheckCredentials(t *testing.T) {
 			url:           "core_url",
 			componentName: "Core",
 			config: map[string]string{
-				"default.core_url": "http://localhost:12010/v2",
+				"default.core_url": "http://localhost:12010",
 				"default.core_key": "http://discovery.core.cn",
 			},
 			err: nil,
