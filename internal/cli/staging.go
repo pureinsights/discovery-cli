417ac536a6e532a348343de382b6c1c38ff69101
diegogranados-pi <diego.granados@pureinsights.com>
2025-12-19 14:37:23 -0600

    PDP-3453: Revert changes

--- internal/cli/staging.go
+++ internal/cli/staging.go
@@ -13,7 +13,7 @@ type StagingBucketController interface {
 	Create(bucket string, options gjson.Result) (gjson.Result, error)
 	Get(bucket string) (gjson.Result, error)
 	Delete(bucket string) (gjson.Result, error)
-	CreateIndex(bucket, index string, config gjson.Result) (gjson.Result, error)
+	CreateIndex(bucket, index string, config []gjson.Result) (gjson.Result, error)
 }
 
 // StoreBucket creates or updates a bucket if it exists. It receives an options parameter that contains the configuration of the bucket.
@@ -34,16 +34,16 @@ func (d discovery) StoreBucket(client StagingBucketController, bucketName string
 			indices := options.Get("indices").Array()
 			for _, index := range indices {
 				indexName := index.Get("name").String()
-				// indexConfig := []gjson.Result{}
-				// for _, field := range index.Get("fields").Array() {
-				// 	fieldJson, err := sjson.Set("{}", field.Get("key").String(), field.Get("value").String())
-				// 	if err != nil {
-				// 		return NewErrorWithCause(ErrorExitCode, err, "Could not update index with name %q of bucket %q.", indexName, bucketName)
-				// 	}
-				// 	indexConfig = append(indexConfig, gjson.Parse(fieldJson))
-				// }
+				indexConfig := []gjson.Result{}
+				for _, field := range index.Get("fields").Array() {
+					fieldJson, err := sjson.Set("{}", field.Get("key").String(), field.Get("value").String())
+					if err != nil {
+						return NewErrorWithCause(ErrorExitCode, err, "Could not update index with name %q of bucket %q.", indexName, bucketName)
+					}
+					indexConfig = append(indexConfig, gjson.Parse(fieldJson))
+				}
 
-				indexAck, err := client.CreateIndex(bucketName, indexName, index)
+				indexAck, err := client.CreateIndex(bucketName, indexName, indexConfig)
 				if err != nil {
 					return NewErrorWithCause(ErrorExitCode, err, "Could not update index with name %q of bucket %q.", indexName, bucketName)
 				}
