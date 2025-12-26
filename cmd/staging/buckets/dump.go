4e97f74df00054acc8a40485f48c4a3cd8386efb
Diego Granados <diego.granados@pureinsights.com>
2025-12-26 15:18:32 -0600

    PDP-3458: Fix help

--- cmd/staging/buckets/dump.go
+++ cmd/staging/buckets/dump.go
@@ -16,7 +16,7 @@ func NewDumpCommand(d cli.Discovery) *cobra.Command {
 	dump := &cobra.Command{
 		Use:   "dump <bucketName>",
 		Short: "The command that dumps buckets to Discovery Staging.",
-		Long:  "dump is the command used to scroll a bucket's content in the Discovery Staging Repository. The bucket's name is sent as the mandatory argument. The user can send filters with the --filter flag, which is a single JSON string that contains all of the filters. With the --projections flag, the user can send the fields that will be included or excluded from the results. With the --max flag, the user can send the maximum number of elements that will be retrieved with every page.",
+		Long:  "dump is the command used to scroll a bucket's content in the Discovery Staging Repository. The bucket's name is sent as the mandatory argument. The user can send filters with the --filter flag, which is a single JSON string that contains all of the filters. With the --projection flag, the user can send the fields that will be included or excluded from the results. With the --max flag, the user can send the maximum number of elements that will be retrieved with every page.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			profile, err := cmd.Flags().GetString("profile")
 			if err != nil {
@@ -50,7 +50,7 @@ func NewDumpCommand(d cli.Discovery) *cobra.Command {
 		},
 		Args: cobra.ExactArgs(1),
 		Example: `	# Dump a bucket with filters, include projections, and a max page size of 5
-	discovery staging bucket dump my-bucket -f '{"equals":{"field":"my-field","value":"my-value"}}' --projections '{"includes":["my-field","my-field-2"]}' --max 5`,
+	discovery staging bucket dump my-bucket -f '{"equals":{"field":"my-field","value":"my-value"}}' --projection '{"includes":["my-field","my-field-2"]}' --max 5`,
 	}
 
 	dump.Flags().StringVarP(&filters, "filter", "f", "", "the DSL containing the filters that will be applied to the scroll")
