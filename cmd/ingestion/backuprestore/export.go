66d422743fb402e7e360859802a91eece1f3a0a1
Diego Granados <diego.granados@pureinsights.com>
2026-01-05 15:38:07 -0600

    PDP-3487: Add arguments to Ingestion commands

--- cmd/ingestion/backuprestore/export.go
+++ cmd/ingestion/backuprestore/export.go
@@ -13,7 +13,7 @@ import (
 func NewExportCommand(d cli.Discovery) *cobra.Command {
 	var file string
 	export := &cobra.Command{
-		Use:   "export [subcommands]",
+		Use:   "export",
 		Short: "Export all of Discovery Ingestion's entities",
 		Long:  fmt.Sprintf(commands.LongExport, "Ingestion"),
 		RunE: func(cmd *cobra.Command, args []string) error {
