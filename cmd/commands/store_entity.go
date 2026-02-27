5b83074107396188ac9600c3e411b3d8fc8d92a7
Diego Granados <diego.granados@pureinsights.com>
2026-02-27 09:40:27 -0600

    PDP-3529: Remove duplicated text

--- cmd/commands/store_entity.go
+++ cmd/commands/store_entity.go
@@ -10,6 +10,8 @@ import (
 const (
 	// LongStore is the message used in the Long field of the Store commands.
 	LongStore string = "store is the command used to create and update Discovery %[2]s's %[1]ss. With the --data flag, the user can send a single JSON configuration or an array to upsert multiple %[1]s. On the other hand, the user can also send multiple arguments with the paths of files that contain JSON configurations. Each of these files will be processed individually, but all entities will be upserted. The --data flag and file arguments are required, but mutually exclusive. The user can only send the data flag or file arguments, not both at the same time."
+	// DataEmptyError is the error message when the given data is empty.
+	DataEmptyError string = "Data cannot be empty"
 )
 
 // StoreCommandConfig contains the parameters sent to the StoreCommand function.
@@ -39,7 +41,7 @@ func readDataFromFiles(file string) (gjson.Result, error) {
 	}
 
 	if len(jsonBytes) == 0 || string(jsonBytes) == "" {
-		return gjson.Result{}, cli.NewError(cli.ErrorExitCode, "Data cannot be empty")
+		return gjson.Result{}, cli.NewError(cli.ErrorExitCode, DataEmptyError)
 	}
 
 	return gjson.ParseBytes(jsonBytes), nil
@@ -77,7 +79,7 @@ func StoreCommand(d cli.Discovery, client cli.Creator, config storeCommandConfig
 		return nil
 	} else {
 		if config.data == "" {
-			return cli.NewError(cli.ErrorExitCode, "Data cannot be empty")
+			return cli.NewError(cli.ErrorExitCode, DataEmptyError)
 		}
 
 		return d.UpsertEntities(client, gjson.Parse(config.data), config.abortOnError, printer)
@@ -116,7 +118,7 @@ func SearchStoreCommand(d cli.Discovery, client cli.SearchCreator, config storeC
 		return nil
 	} else {
 		if config.data == "" {
-			return cli.NewError(cli.ErrorExitCode, "Data cannot be empty")
+			return cli.NewError(cli.ErrorExitCode, DataEmptyError)
 		}
 
 		return d.SearchUpsertEntities(client, gjson.Parse(config.data), config.abortOnError, printer)
