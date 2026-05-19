96b4e6d03de716b86bb4cae745dfc176b0097be3
Diego Granados <diego.granados@pureinsights.com>
2026-05-19 15:03:24 -0600

    PDP-3592: Fix comment

--- cmd/queryflow/mcpservers/tools/store.go
+++ cmd/queryflow/mcpservers/tools/store.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// NewStoreCommand creates the mcp-server store command.
+// NewStoreCommand creates the mcp-server tool store command.
 func NewStoreCommand(d cli.Discovery) *cobra.Command {
 	var abortOnError bool
 	var data string
